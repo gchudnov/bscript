b1ff3231557e4a4846d9b62111e7d52db749c945
Grigorii Chudnov <g.chudnov@gmail.com>
2022-12-13 21:20:09 +0100

    upd code

---
+++ builder/src/test/scala/com/github/gchudnov/bscript/builder/state/ScopeBuilderSpec.scala
@@ -0,0 +1,13 @@
+package com.github.gchudnov.bscript.builder.state
+
+import com.github.gchudnov.bscript.builder.TestSpec
+
+final class ScopeBuilderSpec extends TestSpec:
+
+  "ScopeBuilderSpec" when {
+    "xxx" should {
+      "yyy" in {
+        
+      }
+    }
+  }
