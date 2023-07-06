27503c74cd84d1117ccf02e9cffc002ecec1fcc5
Grigorii Chudnov <g.chudnov@gmail.com>
2023-07-07 00:25:20 +0200

    upd read and write path

--- builder/src/main/scala/com/github/gchudnov/bscript/builder/env/HasScopeAsts.scala
+++ builder/src/main/scala/com/github/gchudnov/bscript/builder/env/HasScopeAsts.scala
@@ -1,6 +1,14 @@
 package com.github.gchudnov.bscript.builder.env
 
 import com.github.gchudnov.bscript.builder.state.ScopeAsts
+import com.github.gchudnov.bscript.builder.state.ReadScopeAsts
+import com.github.gchudnov.bscript.builder.state.WriteScopeAsts
+
+trait HasReadScopeAsts:
+  def scopeAsts: ReadScopeAsts
+
+trait HasWriteScopeAsts:
+  def scopeAsts: WriteScopeAsts
 
 trait HasScopeAsts:
   def scopeAsts: ScopeAsts
